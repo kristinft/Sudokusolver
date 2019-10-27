eba0f0277d3079b3c7295556d2c5fbf29090552a
Aksel Sundal <aksel.sundal@gmail.com>
2019-10-27 02:13:29 +0200

    laget ferdig logikk

--- Sudokusolver.py
+++ Sudokusolver.py
@@ -2,6 +2,8 @@
 
 board= 9*[9*[0]]
 
+
+
 def check_row(row_pos, col_pos, num):
     legal_row = True
     if num in board[row_pos]:
@@ -19,15 +21,29 @@ def check_col(row_pos, col_pos, num):
     return legal_col
 
 
+def make_box(row_pos, col_pos):
+    box  = []
+    box_dict={1:[(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)], 2:[(0,3), (0,4), (0,5), (1,3), (1,4), (1,5), (2,3), (2,4), (2,5)], }
+
+
 def check_box(row_pos, col_pos, num):
     legal_box = True
+    coordinate=row_pos,col_pos
+    for i in range(9)
+        if coordinate in box_dict{i}:
+            for k in box_dict{i}:
+                row_pos,col_pos=k
+                if board[row_pos][col_pos] == num:
+                    legal_box = False
 
-    if 0 <= row_pos <= 2 and 0 <= col_pos <=2
 
-    return legal_box
+                    return legal_box
 
 
-        
+
+#I pos. (0,0) vil me sjekke board[0][0] til board[0][2], board[1][0] og board[2][0]
+
+
 
 
 
@@ -36,18 +52,18 @@ def check_legal(row_pos,col_pos,num):
     for row_pos in range(0,9):
         for col_pos in range(0,9):
 
-            num_list=()
-            for num in range (1,10):
-                if board[row_pos][col_pos] != 0 :
+            num_list=[]
+            if board[row_pos][col_pos] == 0 :
+                for num in range (1,10):
                     legal_row=check_row(row_pos, col_pos, num)
                     legal_col=check_col(row_pos, col_pos,num)
                     legal_box=check_box(row_pos, col_pos, num)
 
-                    if legal_row ==False and legal_col==False and legal_box==False:
+                    if legal_row ==True and legal_col==True and legal_box==True:
                         num_list +=[num]
 
-                    if len(num_list) ==1:
-                        board[row_pos][col_pos] = num
+            if len(num_list) == 1:
+                board[row_pos][col_pos] = num_list[0]
 
 
 print(board)
